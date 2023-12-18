8b596cbef49edc01a987eb48523246a412773e0b
shree-gjn <shree.hari@g-japan.com>
2023-12-18 15:37:28 +0530

    ui components 181223

--- src/pages/message.js
+++ src/pages/message.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
 import BottomNav from '../components/BottomNav';
 
 const MessageComponent = ({ handleNext }) => {
+  const [value, setValue] = React.useState('1');
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
-    <div>
-      <h1>Message</h1>
-      {/* Your content goes here */}
-      <BottomNav />
+    <Box sx={{ width: '100%', typography: 'body1' }}>
+    <div className="PageHeader">
+      <p>メッセージ</p>
     </div>
+    <TabContext value={value}>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <TabList onChange={handleChange} aria-label="lab API tabs example">
+          <Tab label="シークレットエントリー" value="1" sx={{width:'50%'}}/>
+          <Tab label="企業スカウト" value="2" sx={{width:'50%'}}/>
+        </TabList>
+      </Box>
+      <TabPanel value="1">Item One</TabPanel>
+      <TabPanel value="2">Item Two</TabPanel>
+    </TabContext>
+    <BottomNav />
+  </Box>
   );
 };
 
--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
 import BottomNav from '../components/BottomNav';
 
-const ProfileComponent = () => {
+const ProfileComponent = ({ handleNext }) => {
+  const [value, setValue] = React.useState('1');
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
-    <div>
-      <h1> Profile </h1>
-      <BottomNav />
+    <Box sx={{ width: '100%', typography: 'body1' }}>
+    <div className="PageHeader">
+      <p>プロフィール</p>
     </div>
+    <TabContext value={value}>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <TabList onChange={handleChange} aria-label="lab API tabs example">
+          <Tab label="履歴書・職務経歴書" value="1" sx={{width:'50%'}}/>
+          <Tab label="設定" value="2" sx={{width:'50%'}}/>
+        </TabList>
+      </Box>
+      <TabPanel value="1">Item One</TabPanel>
+      <TabPanel value="2">Item Two</TabPanel>
+    </TabContext>
+    <BottomNav />
+  </Box>
   );
 };
 
