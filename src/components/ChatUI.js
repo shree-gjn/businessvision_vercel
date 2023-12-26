bda8d525e7ca710ddf13b4b718f6a4ae87b80ac3
shree-gjn <shree.hari@g-japan.com>
2023-12-27 04:37:38 +0530

    inprogress chat

--- src/components/ChatUI.js
+++ src/components/ChatUI.js
@@ -1,16 +1,37 @@
 import React, { useState } from 'react';
-import { Avatar, TextField, Paper, Grid, InputAdornment, IconButton } from '@mui/material';
+import { Avatar, TextField, Paper, Grid, InputAdornment, IconButton, Typography } from '@mui/material';
 import SendIcon from '@mui/icons-material/Send';
+import AttachFileIcon from '@mui/icons-material/AttachFile';
 
 const ChatComponent = () => {
   const [messages, setMessages] = useState([]);
   const [newMessage, setNewMessage] = useState('');
+  const fileInputRef = React.createRef();
+
+  const getCurrentTime = () => {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    return `${hours}:${minutes}`;
+  };
 
   const handleSendMessage = () => {
     if (newMessage.trim() !== '') {
-      setMessages([...messages, { text: newMessage, sender: 'user' }]);
+      const timestamp = getCurrentTime();
+      const message = { text: newMessage, sender: 'user', timestamp };
+      setMessages([...messages, message]);
       setNewMessage('');
     }
+  };
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    // Handle the selected file as needed
+    console.log('Selected File:', file);
+  };
+
+  const handleAttachmentClick = () => {
+    fileInputRef.current.click();
   };
 
   return (
@@ -19,22 +40,16 @@ const ChatComponent = () => {
         {/* Messages Container */}
         <Grid item xs={12} style={{ height: '400px', overflowY: 'auto' }}>
           {messages.map((message, index) => (
-            <div key={index} style={{ marginBottom: 8, display: 'flex', alignItems: 'center' }}>
-              {message.sender === 'user' ? (
-                <>
-                  <Avatar src="path_to_user_avatar" alt="User Avatar" />
-                  <div style={{ marginLeft: 8, background: '#e3f2fd', padding: 8, borderRadius: 8 }}>
-                    {message.text}
-                  </div>
-                </>
-              ) : (
-                <>
-                  <div style={{ marginRight: 8, background: '#f0f0f0', padding: 8, borderRadius: 8 }}>
-                    {message.text}
-                  </div>
-                  <Avatar src="path_to_received_avatar" alt="Received Avatar" />
-                </>
-              )}
+            <div key={index} style={{ marginBottom: 8 }}>
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                <Avatar src="path_to_user_avatar" alt="User Avatar" />
+                <div style={{ marginLeft: 8, background: '#e3f2fd', padding: 8, borderRadius: 8 }}>
+                  {message.text}
+                </div>
+              </div>
+              <Typography variant="body2" color="textSecondary" style={{ marginTop: 4, textAlign: 'right' }}>
+                {message.timestamp}
+              </Typography>
             </div>
           ))}
         </Grid>
@@ -51,6 +66,9 @@ const ChatComponent = () => {
               InputProps={{
                 endAdornment: (
                   <InputAdornment position="end">
+                    <IconButton onClick={handleAttachmentClick} edge="end">
+                      <AttachFileIcon />
+                    </IconButton>
                     <IconButton onClick={handleSendMessage} edge="end">
                       <SendIcon />
                     </IconButton>
@@ -59,6 +77,14 @@ const ChatComponent = () => {
               }}
             />
           </Grid>
+          {/* Hidden file input */}
+          <input
+            type="file"
+            accept="image/*, video/*, audio/*, .pdf, .doc, .docx"
+            style={{ display: 'none' }}
+            ref={fileInputRef}
+            onChange={handleFileChange}
+          />
         </Grid>
       </Grid>
     </div>
