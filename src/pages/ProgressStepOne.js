b5140c1fb091d4c50ab07796613908c51b352343
shree-gjn <shree.hari@g-japan.com>
2023-12-26 17:25:12 +0530

    inprogress stepper

--- src/pages/ProgressStepOne.js
+++ src/pages/ProgressStepOne.js
@@ -1,14 +1,187 @@
 import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import { Link } from 'react-router-dom';
+import {ReactComponent as BuildingIcon} from '../assets/BuildingIcon.svg';
+import {ReactComponent as BagIcon} from '../assets/BagIcon.svg';
 import BottomNav from '../components/BottomNav';
 
-const ProgressStepOne = () => {
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    boxShadow: 'none', 
+  }));
+  
+
+export default function ProgressStepOne() {
   return (
     <>
-    <h1> Step One </h1>
-    <BottomNav />
-   </>
-  );
-  
-};
+    <Card sx={{ minWidth: 275, marginBottom:'30px' }}>
+      <CardContent>
+        <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={1}>
+            <Grid item xs={6}>
+            </Grid>
+            <Grid item xs={2}>
+            </Grid>
+            <Grid item xs={4}>
+            <Item sx={{fontSize:'12px', background:'rgba(212, 184, 107, 0.54)', marginBottom:'5px'}}>応募済み</Item>
+            </Grid>
+        </Grid>
+        </Box>
+        <Typography variant="h6" component="div" sx={{fontSize:'14px', fontWeight:'700', textAlign:'left'}}>
+        【渋谷/プライム上場】SaaS/ASPのパイオニア企業での経理マネージャー/リモート有
+        </Typography>
+        <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={1} sx={{marginTop:'10px'}}>
+            <Grid item xs={6}>
+            <Item sx={{textAlign:'left', display:'flex', gap:'5px', paddingTop:'0px'}}> 
+              <BuildingIcon /> <Typography variant="body1" sx={{fontSize:'12px', color:'#16375A', fontWeight:'500'}}> 株式会社ABC </Typography>​</Item>
+            </Grid>
+            <Grid item xs={6} sx={{paddingTop:'0px'}}>
+            <Item sx={{textAlign:'left', display:'flex', gap:'5px', paddingTop:'0px'}}> 
+              <BagIcon /> <Typography variant="body1" sx={{fontSize:'12px', color:'#16375A', fontWeight:'500'}}> 経理マネジャー  </Typography>​​</Item>
+            </Grid>
+        </Grid>
+        </Box>
+      </CardContent>
+    </Card>
 
-export default ProgressStepOne;
+    <Card sx={{ minWidth: 275, marginBottom:'30px' }}>
+      <CardContent>
+        <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={1}>
+            <Grid item xs={6}>
+            </Grid>
+            <Grid item xs={2}>
+            </Grid>
+            <Grid item xs={4}>
+            <Item sx={{fontSize:'12px', background:'rgba(212, 184, 107, 0.54)', marginBottom:'5px'}}>応募済み</Item>
+            </Grid>
+        </Grid>
+        </Box>
+        <Typography variant="h6" component="div" sx={{fontSize:'14px', fontWeight:'700', textAlign:'left'}}>
+        【渋谷/プライム上場】SaaS/ASPのパイオニア企業での経理マネージャー/リモート有
+        </Typography>
+        <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={1} sx={{marginTop:'10px'}}>
+            <Grid item xs={6}>
+            <Item sx={{textAlign:'left', display:'flex', gap:'5px', paddingTop:'0px'}}> 
+              <BuildingIcon /> <Typography variant="body1" sx={{fontSize:'12px', color:'#16375A', fontWeight:'500'}}> 株式会社ABC </Typography>​</Item>
+            </Grid>
+            <Grid item xs={6} sx={{paddingTop:'0px'}}>
+            <Item sx={{textAlign:'left', display:'flex', gap:'5px', paddingTop:'0px'}}> 
+              <BagIcon /> <Typography variant="body1" sx={{fontSize:'12px', color:'#16375A', fontWeight:'500'}}> 経理マネジャー  </Typography>​​</Item>
+            </Grid>
+        </Grid>
+        </Box>
+      </CardContent>
+    </Card>
+
+    <Card sx={{ minWidth: 275, marginBottom:'30px' }}>
+      <CardContent>
+        <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={1}>
+            <Grid item xs={6}>
+            </Grid>
+            <Grid item xs={2}>
+            </Grid>
+            <Grid item xs={4}>
+            <Item sx={{fontSize:'12px', background:'rgba(212, 184, 107, 0.54)', marginBottom:'5px'}}>応募済み</Item>
+            </Grid>
+        </Grid>
+        </Box>
+        <Typography variant="h6" component="div" sx={{fontSize:'14px', fontWeight:'700', textAlign:'left'}}>
+        【渋谷/プライム上場】SaaS/ASPのパイオニア企業での経理マネージャー/リモート有
+        </Typography>
+        <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={1} sx={{marginTop:'10px'}}>
+            <Grid item xs={6}>
+            <Item sx={{textAlign:'left', display:'flex', gap:'5px', paddingTop:'0px'}}> 
+              <BuildingIcon /> <Typography variant="body1" sx={{fontSize:'12px', color:'#16375A', fontWeight:'500'}}> 株式会社ABC </Typography>​</Item>
+            </Grid>
+            <Grid item xs={6} sx={{paddingTop:'0px'}}>
+            <Item sx={{textAlign:'left', display:'flex', gap:'5px', paddingTop:'0px'}}> 
+              <BagIcon /> <Typography variant="body1" sx={{fontSize:'12px', color:'#16375A', fontWeight:'500'}}> 経理マネジャー  </Typography>​​</Item>
+            </Grid>
+        </Grid>
+        </Box>
+      </CardContent>
+    </Card>
+
+    <Card sx={{ minWidth: 275, marginBottom:'30px' }}>
+      <CardContent>
+        <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={1}>
+            <Grid item xs={6}>
+            </Grid>
+            <Grid item xs={2}>
+            </Grid>
+            <Grid item xs={4}>
+            <Item sx={{fontSize:'12px', background:'rgba(212, 184, 107, 0.54)', marginBottom:'5px'}}>応募済み</Item>
+            </Grid>
+        </Grid>
+        </Box>
+        <Typography variant="h6" component="div" sx={{fontSize:'14px', fontWeight:'700', textAlign:'left'}}>
+        【渋谷/プライム上場】SaaS/ASPのパイオニア企業での経理マネージャー/リモート有
+        </Typography>
+        <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={1} sx={{marginTop:'10px'}}>
+            <Grid item xs={6}>
+            <Item sx={{textAlign:'left', display:'flex', gap:'5px', paddingTop:'0px'}}> 
+              <BuildingIcon /> <Typography variant="body1" sx={{fontSize:'12px', color:'#16375A', fontWeight:'500'}}> 株式会社ABC </Typography>​</Item>
+            </Grid>
+            <Grid item xs={6} sx={{paddingTop:'0px'}}>
+            <Item sx={{textAlign:'left', display:'flex', gap:'5px', paddingTop:'0px'}}> 
+              <BagIcon /> <Typography variant="body1" sx={{fontSize:'12px', color:'#16375A', fontWeight:'500'}}> 経理マネジャー  </Typography>​​</Item>
+            </Grid>
+        </Grid>
+        </Box>
+      </CardContent>
+    </Card>
+
+    <Card sx={{ minWidth: 275, marginBottom:'30px' }}>
+      <CardContent>
+        <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={1}>
+            <Grid item xs={6}>
+            </Grid>
+            <Grid item xs={2}>
+            </Grid>
+            <Grid item xs={4}>
+            <Item sx={{fontSize:'12px', background:'rgba(212, 184, 107, 0.54)', marginBottom:'5px'}}>応募済み</Item>
+            </Grid>
+        </Grid>
+        </Box>
+        <Typography variant="h6" component="div" sx={{fontSize:'14px', fontWeight:'700', textAlign:'left'}}>
+        【渋谷/プライム上場】SaaS/ASPのパイオニア企業での経理マネージャー/リモート有
+        </Typography>
+        <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={1} sx={{marginTop:'10px'}}>
+            <Grid item xs={6}>
+            <Item sx={{textAlign:'left', display:'flex', gap:'5px', paddingTop:'0px'}}> 
+              <BuildingIcon /> <Typography variant="body1" sx={{fontSize:'12px', color:'#16375A', fontWeight:'500'}}> 株式会社ABC </Typography>​</Item>
+            </Grid>
+            <Grid item xs={6} sx={{paddingTop:'0px'}}>
+            <Item sx={{textAlign:'left', display:'flex', gap:'5px', paddingTop:'0px'}}> 
+              <BagIcon /> <Typography variant="body1" sx={{fontSize:'12px', color:'#16375A', fontWeight:'500'}}> 経理マネジャー  </Typography>​​</Item>
+            </Grid>
+        </Grid>
+        </Box>
+      </CardContent>
+    </Card>
+    
+    <>
+    <BottomNav />
+    </>
+  </>
+  );
+}
--- src/pages/SecretEntry.js
+++ src/pages/SecretEntry.js
@@ -2,16 +2,13 @@ import * as React from 'react';
 import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import { Link } from 'react-router-dom';
 import {ReactComponent as BuildingIcon} from '../assets/BuildingIcon.svg';
 import {ReactComponent as BagIcon} from '../assets/BagIcon.svg';
-import {ReactComponent as ChatIcon} from '../assets/ChatIcon.svg'; 
 
 const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
